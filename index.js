dc5e06f40623dd79f6365f8688f5dbbeb26b8651
AlvaroP1504 <cod.black.opps0@gmail.com>
2023-08-19 22:19:30 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import { data,data_detail } from "./db/data.js";
+import { PORT } from "./ports.js";
+
+const app = express();
+app.disable('x-powered-by');
+
+
+app.get('/books', (req, res) => {
+    res.json(data);
+});
+
+app.get('/books/details/:id', (req, res) => {
+    const bookId = parseInt(req.params.id);
+    const book = data_detail.find(book => book.id === bookId);
+  
+    if (!book) {
+      res.status(404).json({ message: 'Libro no encontrado' });
+    } else {
+      res.json(book);
+    }
+  });
+
+app.listen(PORT, err => {
+    if (err) {
+        console.error("Error escuchando: ", err);
+        return;
+    }
+    console.log(`Escuchando en el puerto: ${PORT}`);
+});
+
+
+
