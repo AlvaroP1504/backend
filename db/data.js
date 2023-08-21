57bdc1f47a19bd964ec011fcf1450637acb20d3d
AlvaroP1504 <cod.black.opps0@gmail.com>
2023-08-20 22:25:45 -0500

    100% backend

--- db/data.js
+++ db/data.js
@@ -1,140 +1,140 @@
 export const data_detail = [
   {
     id: 1,
-    book: "El Secreto de la Montaña",
+    title: "El Secreto de la Montaña",
     description: "Una emocionante novela de aventuras en las altas montañas.",
     autor: "Ana García",
     genre: "Aventura"
   },
   {
     id: 2,
-    book: "La Ciudad de los Sueños",
+    title: "La Ciudad de los Sueños",
     description: "Explora una ciudad mágica donde los sueños se convierten en realidad.",
     autor: "Carlos Martínez",
     genre: "Fantasía"
   },
   {
     id: 3,
-    book: "Crimen en el Expreso de Medianoche",
+    title: "Crimen en el Expreso de Medianoche",
     description: "Un famoso detective debe resolver un misterioso asesinato a bordo de un tren.",
     autor: "María López",
     genre: "Misterio"
   },
   {
     id: 4,
-    book: "Amores en París",
+    title: "Amores en París",
     description: "Una historia romántica que transcurre en las calles de la ciudad del amor.",
     autor: "Juan Rodríguez",
     genre: "Romance"
   },
   {
     id: 5,
-    book: "El Laboratorio de la Alquimia",
+    title: "El Laboratorio de la Alquimia",
     description: "Un joven aprendiz de alquimista busca descubrir los secretos de la transformación.",
     autor: "Sofía Fernández",
     genre: "Fantasía"
   },
   {
       id: 6,
-      book: "Los Guardianes del Bosque",
+      title: "Los Guardianes del Bosque",
       description: "Una historia épica de valientes guardianes que protegen un antiguo bosque.",
       autor: "Luisa García",
       genre: "Fantasía"
     },
     {
       id: 7,
-      book: "El Misterio de la Mansión Abandonada",
+      title: "El Misterio de la Mansión Abandonada",
       description: "Un grupo de amigos debe resolver los enigmas de una mansión supuestamente embrujada.",
       autor: "Diego Martínez",
       genre: "Misterio"
     },
     {
       id: 8,
-      book: "Amores Prohibidos",
+      title: "Amores Prohibidos",
       description: "Una pasión intensa surge entre dos amantes cuyos destinos están entrelazados por el destino.",
       autor: "Elena Pérez",
       genre: "Romance"
     },
     {
       id: 9,
-      book: "La Búsqueda del Tesoro Perdido",
+      title: "La Búsqueda del Tesoro Perdido",
       description: "Un grupo de aventureros se embarca en una emocionante búsqueda de un tesoro legendario.",
       autor: "Ricardo Sánchez",
       genre: "Aventura"
     },
     {
       id: 10,
-      book: "El Secreto de la Antigua Profecía",
+      title: "El Secreto de la Antigua Profecía",
       description: "Un joven destinado a cumplir una antigua profecía debe enfrentarse a su destino.",
       autor: "Laura Ramírez",
       genre: "Fantasía"
     },
     {
       id: 11,
-      book: "Código de Silencio",
+      title: "Código de Silencio",
       description: "Un thriller emocionante sobre un agente encubierto atrapado en una red de conspiración.",
       autor: "Roberto Fernández",
       genre: "Suspenso"
     },
     {
       id: 12,
-      book: "Amistad en Tiempos de Guerra",
+      title: "Amistad en Tiempos de Guerra",
       description: "La conmovedora historia de dos amigos que luchan por mantener su amistad en medio de la guerra.",
       autor: "Ana López",
       genre: "Drama"
     },
     {
       id: 13,
-      book: "El Enigma del Espejo Mágico",
+      title: "El Enigma del Espejo Mágico",
       description: "Un espejo mágico oculta secretos oscuros que deben ser desentrañados por un valiente aventurero.",
       autor: "Javier Ruiz",
       genre: "Fantasía"
     },
     {
       id: 14,
-      book: "La Última Esperanza",
+      title: "La Última Esperanza",
       description: "En un mundo devastado, un grupo de sobrevivientes busca una última oportunidad para la humanidad.",
       autor: "María Sánchez",
       genre: "Ciencia Ficción"
     },
     {
       id: 15,
-      book: "La Sombra del Pasado",
+      title: "La Sombra del Pasado",
       description: "Un oscuro secreto del pasado regresa para atormentar a los habitantes de un pequeño pueblo.",
       autor: "Carlos Ramírez",
       genre: "Suspense"
     },
     {
       id: 16,
-      book: "El Legado de los Ancestros",
+      title: "El Legado de los Ancestros",
       description: "Un joven descubre que su linaje está vinculado a un antiguo poder que debe proteger.",
       autor: "Isabel Martínez",
       genre: "Fantasía"
     },
     {
       id: 17,
-      book: "El Misterio de la Rosa Negra",
+      title: "El Misterio de la Rosa Negra",
       description: "Un detective se enfrenta a un enigma inexplicable relacionado con una misteriosa rosa.",
       autor: "Daniel Rodríguez",
       genre: "Misterio"
     },
     {
       id: 18,
-      book: "Amor en la Ciudad Eterna",
+      title: "Amor en la Ciudad Eterna",
       description: "Dos almas perdidas se encuentran y se enamoran en las románticas calles de Roma.",
       autor: "Carmen López",
       genre: "Romance"
     },
     {
       id: 19,
-      book: "El Oráculo de los Elementos",
+      title: "El Oráculo de los Elementos",
       description: "Un grupo de elegidos debe usar su conexión con los elementos para salvar el mundo.",
       autor: "Fernando Sánchez",
       genre: "Fantasía"
     },
     {
       id: 20,
-      book: "La Caza del Tesoro Perdido",
+      title: "La Caza del Tesoro Perdido",
       description: "Un emocionante concurso de caza del tesoro lleva a los participantes a lugares inexplorados.",
       autor: "Alejandra García",
       genre: "Aventura"
@@ -143,102 +143,102 @@ export const data_detail = [
 export const data = [
   {
     id: 1,
-    book: "El Secreto de la Montaña",
+    title: "El Secreto de la Montaña",
     genre: "Aventura"
   },
   {
     id: 2,
-    book: "La Ciudad de los Sueños",
+    title: "La Ciudad de los Sueños",
     genre: "Fantasía"
   },
   {
     id: 3,
-    book: "Crimen en el Expreso de Medianoche",
+    title: "Crimen en el Expreso de Medianoche",
     genre: "Misterio"
   },
   {
     id: 4,
-    book: "Amores en París",
+    title: "Amores en París",
     genre: "Romance"
   },
   {
     id: 5,
-    book: "El Laboratorio de la Alquimia",
+    title: "El Laboratorio de la Alquimia",
     genre: "Fantasía"
   },
   {
       id: 6,
-      book: "Los Guardianes del Bosque",
+      title: "Los Guardianes del Bosque",
       genre: "Fantasía"
     },
     {
       id: 7,
-      book: "El Misterio de la Mansión Abandonada",
+      title: "El Misterio de la Mansión Abandonada",
       genre: "Misterio"
     },
     {
       id: 8,
-      book: "Amores Prohibidos",
+      title: "Amores Prohibidos",
       genre: "Romance"
     },
     {
       id: 9,
-      book: "La Búsqueda del Tesoro Perdido",
+      title: "La Búsqueda del Tesoro Perdido",
       genre: "Aventura"
     },
     {
       id: 10,
-      book: "El Secreto de la Antigua Profecía",
+      title: "El Secreto de la Antigua Profecía",
       genre: "Fantasía"
     },
     {
       id: 11,
-      book: "Código de Silencio",
+      title: "Código de Silencio",
       genre: "Suspenso"
     },
     {
       id: 12,
-      book: "Amistad en Tiempos de Guerra",
+      title: "Amistad en Tiempos de Guerra",
       genre: "Drama"
     },
     {
       id: 13,
-      book: "El Enigma del Espejo Mágico",
+      title: "El Enigma del Espejo Mágico",
       genre: "Fantasía"
     },
     {
       id: 14,
-      book: "La Última Esperanza",
+      title: "La Última Esperanza",
       genre: "Ciencia Ficción"
     },
     {
       id: 15,
-      book: "La Sombra del Pasado",
+      title: "La Sombra del Pasado",
       genre: "Suspense"
     },
     {
       id: 16,
-      book: "El Legado de los Ancestros",
+      title: "El Legado de los Ancestros",
       genre: "Fantasía"
     },
     {
       id: 17,
-      book: "El Misterio de la Rosa Negra",
+      title: "El Misterio de la Rosa Negra",
       genre: "Misterio"
     },
     {
       id: 18,
-      book: "Amor en la Ciudad Eterna",
+      title: "Amor en la Ciudad Eterna",
       genre: "Romance"
     },
     {
       id: 19,
-      book: "El Oráculo de los Elementos",
+      title: "El Oráculo de los Elementos",
       genre: "Fantasía"
     },
     {
       id: 20,
-      book: "La Caza del Tesoro Perdido",
+      title: "La Caza del Tesoro Perdido",
       genre: "Aventura"
     }
 ]
--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import express from "express";
+import cors from 'cors'
+
 import { data,data_detail } from "./db/data.js";
 import { PORT } from "./ports.js";
 
 const app = express();
+app.use(cors())
+
 app.disable('x-powered-by');
 
 
@@ -15,7 +19,7 @@ app.get('/books/details/:id', (req, res) => {
     const book = data_detail.find(book => book.id === bookId);
   
     if (!book) {
-      res.status(404).json({ message: 'Libro no encontrado' });
+      res.status(404).json({ error: 'Libro no encontrado' });
     } else {
       res.json(book);
     }
